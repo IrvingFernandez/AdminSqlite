69d2d1640cf6adb71028dc016350b7b739a64389
irving fernandez <ir.ving_11@hotmail.comcd Desktop/git init cd dircd /Users/irvingguapo/Desktop/jaki git commit -m proyecto>
2020-10-06 16:25:37 -0500

    Initial Commit

---
+++ AdminSqlite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdminSqlite
+//
+//  Created by Irving Guapo on 06/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
